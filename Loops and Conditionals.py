6b7b18db347e0c923ad0f6364dcd53309638386c
YashashwiniESPL9260 <134257816+YashashwiniESPL9260@users.noreply.github.com>
2023-08-28 15:39:38 +0530

    Create Loops and Conditionals.py
    Exercise: Loops and Conditionals
    - Create a number guessing game where the computer generates a random number, and the user will give some input to guess it.
    - Provide feedback to the user whether their guess is too high or too low.

---
+++ Loops and Conditionals.py
@@ -0,0 +1,47 @@
+import random
+import math
+
+''' Exercise: Loops and Conditionals
+- Create a number guessing game where the computer generates a random number, and the user will give some input to guess it.
+- Provide feedback to the user whether their guess is too high or too low.'''
+
+# Taking Inputs
+lower = int(input("Enter lowest number:- "))
+
+# Taking Inputs
+upper = int(input("Enter highest number:- "))
+
+# generating random number between
+# the lower and upper
+x = random.randint(lower, upper)
+print("\n\tYou've only ",
+      round(math.log(upper - lower + 1, 2)),
+      " chances to guess the integer!\n")
+
+# Initializing the number of guesses.
+count = 0
+
+# for calculation of minimum number of
+# guesses depends upon range
+while count < math.log(upper - lower + 1, 2):
+    count += 1
+
+    # taking guessing number as input
+    guess = int(input("Guess a number:- "))
+
+    # Condition testing
+    if x == guess:
+        print("Congratulations you did it in ",
+              count, " try")
+        # Once guessed, loop will break
+        break
+    elif x > guess:
+        print("You guessed too small!")
+    elif x < guess:
+        print("You Guessed too high!")
+
+# If Guessing is more than required guesses,
+# shows this output.
+if count >= math.log(upper - lower + 1, 2):
+    print("\nThe number is %d" % x)
+    print("\tBetter Luck Next time!")

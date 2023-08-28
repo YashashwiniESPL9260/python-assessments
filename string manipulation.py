c749cc3f34f7d9effe29bc25f56ac53bcd6b206b
YashashwiniESPL9260 <134257816+YashashwiniESPL9260@users.noreply.github.com>
2023-08-28 12:00:25 +0530

    Update string manipulation.py

--- string manipulation.py
+++ string manipulation.py
@@ -4,9 +4,19 @@ v = "she been saying that her name is yashu"
 w = "he had had he"
 
 #Implement a function to count the occurrences of a specific word in the sentence.
-print('Number of occurrence of had :', u.count('had'))
-print('Number of occurrence of had :', v.count('yashu'))
-print('Number of occurrence of had :', w.count('he'))
+def word_count(str):
+    counts = dict()
+    words = str.split()
+
+    for word in words:
+        if word in counts:
+            counts[word] += 1
+        else:
+            counts[word] = 1
+
+    return counts
+
+print( word_count('the quick brown fox jumps over the lazy dog.'))
 
 #Write a program to replace a chosen word in the sentence with another word.
 u = "yashu had been saying that he had been there"
